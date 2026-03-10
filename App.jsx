706da15b88522654f078b27ac3900dee5bbcc15a
Sathiyavani11 <146532703+Sathiyavani11@users.noreply.github.com>
2026-03-10 13:35:05 +0530

    Add files via upload
    upload

---
+++ App.jsx
@@ -0,0 +1,27 @@
+﻿import { Navigate, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+import ResourcesPage from './pages/ResourcesPage';
+import InsightsPage from './pages/InsightsPage';
+import LearningPathPage from './pages/LearningPathPage';
+import HelpPage from './pages/HelpPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/learning-path" element={<LearningPathPage />} />
+      <Route path="/help" element={<HelpPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/resources" element={<ResourcesPage />} />
+      <Route path="/insights" element={<InsightsPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+}
+
+export default App;
