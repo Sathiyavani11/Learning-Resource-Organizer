c69c7444422fd3c6abf1f5eecfdfb50fe2b6a393
Sathiyavani11 <146532703+Sathiyavani11@users.noreply.github.com>
2026-03-12 09:54:34 +0530

    uploded

---
+++ App.jsx
@@ -0,0 +1,35 @@
+﻿import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+import ResourcesPage from './pages/ResourcesPage';
+import InsightsPage from './pages/InsightsPage';
+import LearningPathPage from './pages/LearningPathPage';
+import HelpPage from './pages/HelpPage';
+import BeginnerHtmlPage from './pages/BeginnerHtmlPage';
+import BeginnerCssPage from './pages/BeginnerCssPage';
+import BeginnerJavaScriptPage from './pages/BeginnerJavaScriptPage';
+
+function App() {
+  return (
+        <BrowserRouter basename="/Learning-Resourse-Oraganizer">
+    <Routes>
+      <Route path="/" index exact element={<LandingPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/learning-path" element={<LearningPathPage />} />
+      <Route path="/beginner/html" element={<BeginnerHtmlPage />} />
+      <Route path="/beginner/css" element={<BeginnerCssPage />} />
+      <Route path="/beginner/javascript" element={<BeginnerJavaScriptPage />} />
+      <Route path="/help" element={<HelpPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/resources" element={<ResourcesPage />} />
+      <Route path="/insights" element={<InsightsPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
